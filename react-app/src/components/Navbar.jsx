06bc091ed689dc9fc566b6458958585448680b9f
ZackCarlstrom <zaca9979@colorado.edu>
2025-04-05 18:04:54 -0600

    Add Icons to Navbar

--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -1,43 +1,49 @@
 import { Link } from "react-router-dom";
+import { FiGrid, FiBookOpen, FiFileText, FiHeart, FiSettings, FiLogOut } from 'react-icons/fi';
+
 
 export default function Navbar() {
     return (
-        <aside class="fixed left-0 top-0 h-full w-64 bg-gray-100 text-black p-5 flex flex-col justify-between">
-        <div>
-            <h2 class="text-xl font-serif font-bold mb-20">Lossless Learning</h2>
-            <h3 class="text-sm font-serif mb-5 text-gray-500">Overview</h3>
-            <nav>
-                <ul>
-                    <li class="mb-2">
-                      <Link to="/" class="underline-hover">Dashboard</Link>
-                      </li>
-                    <li class="mb-2">
-                      <a href="#" class="underline-hover">
-                          Lessons
-                      </a>
-                    </li>
-                    <li class="mb-2">
-                      <Link to="/summary" class="underline-hover">
-                        Summaries
-                      </Link>
-                    </li>
-                    <li class="mb-2">
-                      <a href="#" class="underline-hover">
-                          Favorites
-                      </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div>
-            <h3 class="text-sm mb-5 text-gray-500">Settings</h3>
-            <nav>
-                <ul>
-                    <li class="mb-2"><a href="#" class="underline-hover">Settings</a></li>
-                    <li class="mb-2"><a href="#" class="underline-hover">Logout</a></li>
-                </ul>
-            </nav>
-        </div>
+      <aside className="fixed left-0 top-0 h-full w-64 bg-gray-100 text-black p-5 flex flex-col justify-between">
+      <div>
+        <h2 className="text-xl font-serif font-bold mb-20">Lossless Learning</h2>
+        <h3 className="text-sm font-serif mb-5 text-gray-500">Overview</h3>
+        <nav>
+          <ul>
+            <li className="mb-2 flex items-center gap-1">
+              <FiGrid size={22} className="relative -top-[2px] text-gray-500" />
+              <Link to="/" className="underline-hover">Dashboard</Link>
+            </li>
+            <li className="mb-2 flex items-center gap-1">
+              <FiBookOpen size={22} className="relative -top-[2px] text-gray-500" />
+              <a href="#" className="underline-hover">Lessons</a>
+            </li>
+            <li className="mb-2 flex items-center gap-1">
+              <FiFileText size={22} className="relative -top-[2px] text-gray-500" />
+              <Link to="/summary" className="underline-hover">Summaries</Link>
+            </li>
+            <li className="mb-2 flex items-center gap-2">
+              <FiHeart size={22} className="relative -top-[2px] text-gray-500" />
+              <a href="#" className="underline-hover">Favorites</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h3 className="text-sm mb-5 text-gray-500">Settings</h3>
+        <nav>
+          <ul>
+            <li className="mb-2 flex items-center gap-1">
+              <FiSettings size={22} className="relative -top-[2px] text-gray-500" />
+              <a href="#" className="underline-hover">Settings</a>
+            </li>
+            <li className="mb-2 flex items-center gap-1">
+              <FiLogOut size={22} className="relative -top-[2px] text-gray-500" />
+              <a href="#" className="underline-hover">Logout</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </aside>
     );
 }
