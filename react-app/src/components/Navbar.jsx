aeba1575166901db0c5bc16134b03d2a33dcd03f
ZackCarlstrom <zaca9979@colorado.edu>
2025-04-14 04:52:40 -0600

    Update Spacing and Formatting on Navbar

--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -13,8 +13,8 @@ export default function Navbar() {
       return (
       <aside className="fixed left-0 top-0 h-full w-64 bg-white border-r border-gray-300 text-black p-5 flex flex-col justify-between">
       <div>
-        <h2 className="text-xl font-serif font-bold">Lossless Learning</h2>
-        <nav className="mt-20">
+        <h2 className="text-xl font-serif font-bold mt-8">Lossless Learning</h2>
+        <nav className="mt-10">
         <h3 className="text-m font-serif mb-2 text-emerald-700">Overview</h3>
           <ul>
             <li className="mb-2 flex items-center gap-1">
@@ -36,11 +36,11 @@ export default function Navbar() {
         <h3 className="text-m mb-2 text-emerald-700">Settings</h3>
         <nav>
           <ul>
-            <li className="mb-2 flex items-center gap-1">
+            <li className="flex items-center gap-1 mb-2 ">
               <FiSettings size={22} className="relative -top-[2px] text-gray-500" />
               <a href="#" className="underline-hover">Settings</a>
             </li>
-            <li className="mb-10 flex items-center gap-1">
+            <li className="mb-8 flex items-center gap-1">
               <FiLogOut size={22} className="relative -top-[2px] text-gray-500" />
               <button
                 onClick={() => {
