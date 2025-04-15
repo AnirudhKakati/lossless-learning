abcbe9545691baeeac156c8b3b27efd1c86fbea7
Anirudh Kakati <anirudhkakati@gmail.com>
2025-04-15 00:01:07 -0600

    added docker file and deployment file

--- fast_api_backend/autocomplete_fast_api/main.py
+++ fast_api_backend/autocomplete_fast_api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 import pickle
-
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
 # enable CORS so frontend (on a different domain/port) can access this backend
 app.add_middleware(
@@ -11,11 +11,8 @@ app.add_middleware(
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
 
-
-app = FastAPI()
-
-
-
+#try loading the pre-trained Trie object from a pickle file.
+#if the file does not exist (first run), initialize an empty Trie.
 try:
     with open('trie.pkl', 'rb') as f:
         from trie import Trie
@@ -24,8 +21,17 @@ except FileNotFoundError:
     from trie import Trie
     trie = Trie()
 
-@app.get("/search")
+@app.get("/search/autocomplete")
 def search_most_frequent(phrase: str):
+    """
+    Endpoint to return autocomplete suggestions for a given input phrase.
 
+    Args:
+        phrase (str): The input string (partial phrase) to base autocomplete suggestions on.
+
+    Returns:
+        dict: A dictionary containing a list of suggestion strings under the key "suggestions".
+    """
+    
     suggestions = trie.search_most_frequent(phrase.lower())
     return {"suggestions": suggestions}
